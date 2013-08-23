8766f7a6d33ae367c6fcb834b625f9857a66c760
Mike Hairetis <parallel32@gmail.com>
2013-08-23 09:02:47 -0400

    removed unnecessary deploy-clone.sh

--- config/deploy-clone-dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "sync clone-dev"
-rsync --verbose --progress --delete --exclude "logs" --exclude ".gitignore" --exclude ".git" -rlz /home/ec2-user/staging/ ec2-user@10.28.6.249:/var/www/sawstud.io
