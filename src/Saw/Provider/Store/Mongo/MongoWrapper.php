8ec5a180efe931ced1a4e60c3856398dbae7613d
Mike Hairetis <parallel32@gmail.com>
2013-08-24 04:30:41 -0400

    still mongo secondaries utilization testing

--- src/Saw/Provider/Store/Mongo/MongoWrapper.php
+++ src/Saw/Provider/Store/Mongo/MongoWrapper.php
@@ -167,7 +167,11 @@ class MongoWrapper
 			$collection->setReadPreference(\MongoClient::RP_SECONDARY);
 		else
 			$collection->setReadPreference(\MongoClient::RP_PRIMARY);
-			
+		
+		error_log('---------------------------------------------------------------------');
+		error_log('---------------------------------------------------------------------');
+		error_log('---------------------------------------------------------------------');
+		error_log('connection: '.print_r($this->mongo->getConnections(),true));
 		$cursor = $collection->find($query, $fields);
 
 		if(!empty($sort))
@@ -441,7 +445,7 @@ class MongoWrapper
 			$collection_obj->setReadPreference(\MongoClient::RP_PRIMARY);
 		else
 			$collection_obj->setReadPreference(\MongoClient::RP_SECONDARY);
-		
+
 		$mongo_file_obj = $collection_obj->get($criteria);
 		if(is_object($mongo_file_obj)) // found
 			return $mongo_file_obj;
