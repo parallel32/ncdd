4b5841a43e35cab28af7a4f83a70430d25fa3fb9
Mike Hairetis <parallel32@gmail.com>
2013-10-15 09:51:25 -0400

    finished the non logged in user and redirect post

--- src/views/admin/errors/403.php
+++ src/views/admin/errors/403.php
@@ -9,7 +9,7 @@
       </div>
    </div>
    <form id="saw-form" class="form-horizontal portlet" novalidate="novalidate">
-      <input type="hidden" name="doc[message]" value="The page you're accessing requires authentication.  Please sign-in and you will be redirected back to the page you requested.">
+      <input type="hidden" name="doc[message]" value="The page you're trying to access requires authentication.  Please sign-in and you will be redirected back to the page you requested.">
       <input type="hidden" name="doc[redirect]" value="<?=$_SERVER['REQUEST_URI']?>">
    </form>
 <script>
