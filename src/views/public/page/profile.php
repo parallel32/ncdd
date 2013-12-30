8511feabf695efa94cc9b3c64675de09408ac53c
Mike Hairetis <parallel32@gmail.com>
2013-12-30 11:47:49 -0500

    new profiles incomplete

--- src/views/public/page/profile.php
+++ src/views/public/page/profile.php
@@ -1,3 +1,236 @@
+<script src="js/Bebas_Neue_400.font.js" type="text/javascript"></script>
+    <script src="js/Ubuntu_Condensed_400.font.js" type="text/javascript"></script>
+    <script src="js/Bree_Serif_400.font.js" type="text/javascript"></script>
+    <script type="text/javascript">
+    Cufon.replace('.bc,#mainnav a,.pagetitle,h2,.blue1', { fontFamily: 'Bebas Neue' });
+    Cufon.replace('.uc', { fontFamily: 'Ubuntu Condensed' });
+    Cufon.replace('.bs', { fontFamily: 'Bree Serif' });
+    </script>
+
+<style>
+@font-face {
+  font-family: 'Bree Serif';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Bree Serif'), local('BreeSerif-Regular'), url(http://themes.googleusercontent.com/static/fonts/breeserif/v2/LQ7WLTaITDg4OSRuOZCps73hpw3pgy2gAi-Ip7WPMi0.woff) format('woff');
+}
+.mainMenu .tab-content .tab-pane{display:none}
+
+.mainMenu .fullWidthDropDown.dropdown-menu .close{replace right:0px with left:220px}
+
+.boardCertificationDescr .tab-content > .tab-pane{ display:none;}
+
+.tab-pane.active.text-center{display:none;}
+
+.mainMenu .fullWidthDropDown.dropdown-menu .close{left:26%;}
+.mainMenu .fullWidthDropDown.dropdown-menu {width:100%;}
+.mainMenu .fullWidthDropDown.dropdown-menu #learnTab {
+  margin-top: 17px;
+  margin-right: 0;
+}
+.dropdown-menu.discover.fullWidthDropDown.specialwidthsetting { padding:0 0 0 20px; width:30%;}
+.dropdown-menu.learn.fullWidthDropDown.specialwidthsetting { padding:0 0 0 20px; width:30%;}
+</style>
+<style type="text/css">
+ul.inline{ margin-top: -12px;}
+ul.inline li{ display: inline;}
+span.phone{font-weight: bold;
+  font-size: 25px;
+  color: #2f91e4;}
+  .profilewrap{ border:thin solid #ccc; display: block; min-height: 230px; padding: 10px; margin-top: 40px;}
+  .hrdotted{ margin-top: 0px; }
+  ul.socialicons li {
+display: inline;
+}
+.tr{ text-align: right;}
+.blue1 {
+color: #47769e;
+}
+.orange {
+color: #ff6600;
+font-weight: 500;
+}
+span.orange{ color: #ff6000}
+iframe{width: 98%;}
+.hrdotted {
+border-bottom: thin dashed #ccc;}
+.tc{ text-align: center;}
+#memberprofile{ padding-bottom: 40px;}
+.borderright {
+border-right: thin dashed #ccc;
+}
+#placeholder {
+width: 450px;
+height: 250px;
+}
+#example-4{ visibility: hidden;}
+.pagecontent .container-fluid{ padding: 15px; }
+.duplicate .span12{ margin-left: 0px;}
+.duplicate{ display: none; margin-top: 200px;}
+img.thumbnail{ max-width: 100%;}
+@media( max-width: 400px){
+ #placeholder{ width: 250px; height: 200px}
+}
+@media( max-width: 786px){
+    .tr,.tc { text-align: left;}
+    .contact{ padding: 15px;}
+    .contact .span5{ margin-bottom: 10px}
+    .legend{ display: none;}
+    .profilewrap {
+    border: none;
+    }
+    .borderright{ border-right: none;}
+}
+@media( max-width: 1024px){
+    .legend{ display: none;}
+
+}
+@media( min-width: 768px) and (max-width: 1024px){
+    .phone, .span12.urls{ display: none;}
+    .duplicate{ display: block;}
+
+}
+  </style>
+
+                    <div class="container-fluid" id="memberprofile">
+                      <div class="row-fluid">
+                        <div class="pagecontent">
+                          <div class="profilewrap">
+                            <div class="span2">
+                              <img src="http://placehold.it/160x220" class="thumbnail" alt="profilepic"/>
+                            </div>
+                            <div class="span4 tc">
+                                <h2 class="blue1">ABRAM LINCON</h2>
+                                <span class="orange">GENERAL MEMBER</span>
+                                <div class="clear"></div>
+                                <br>
+                                <img src="img/l1.png" alt="label"> <img src="img/l2.png" alt="label">
+                            </div>
+                            <div class="span6 contact">
+                              <div class="span5">
+                                <ul class="inline">
+                                  <li><img src="img/delegation.png" class=""></li>
+                                  <li><img src="img/delegation.png" class=""></li>
+                                </ul>
+                              </div>
+                              <div class="span7 tr">
+                                <a href="#" ><img src="img/contactme.png" alt="contact"></a><br><br>
+                                <ul class="socialicons">
+                                  <li><img src="img/ln.png" alt="linkedin"></li>
+                                  <li><img src="img/gplus.png" alt="googleplus"></li>
+                                  <li><img src="img/twitter.png" alt="tweets"></li>
+                                  <li><img src="img/fb.png" alt="fb"></li></ul>
+                                  <br>
+                                  <span class="phone bc">Call Now: 800 1231 2800</span>
+                              </div>
+                              <div class="span12 urls">
+                                <br>
+                                <div class="span5">                 
+                                  <a href="#" class="orange"><i class="icon-map-marker"></i> Specialize in DUI</a>
+                                </div>
+                                <div class="span6">
+                                  <a href="#" class="orange"><i class="icon-share orange"></i> www.defensedu.com</a>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="row-fluid duplicate">
+                                <div class="span12">
+                                    <span class="phone span12 bc">Call Now: 800 1231 2800</span>
+                                    <div class="span12">                 
+                                      <a href="#" class="orange"><i class="icon-map-marker"></i> Specialize in DUI</a>
+                                    </div>
+                                    <div class="span12">
+                                      <a href="#" class="orange"><i class="icon-share orange"></i> www.defensedu.com</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+            <div class="clear"></div>
+            <br>
+            <div class="container-fluid">
+              <div class="row-fluid">
+                <div class="span4"><i class="icon-map-marker"></i>  44S Decatur St. motegomry</div>
+                <div class="span4"><i class="icon-share orange"></i> www.defensedu.com</div>
+              </div>
+            </div>
+        <hr class="hrdotted"> 
+        <div class="clear"></div>
+        <div class="container-fluid">
+          <div class="row-fluid">
+           <div class="span6 row1">
+              <h3 class="blue1">Contact Information</h3>
+              <iframe  class="thumbnail" height="140" src="https://maps.google.com/maps?f=d&amp;source=s_d&amp;saddr=445+Decatur+Street+Southeast,+Atlanta,+GA&amp;daddr=&amp;hl=en&amp;geocode=&amp;aq=0&amp;oq=445+S+Decatur+st&amp;sll=32.678125,-83.178297&amp;sspn=6.276919,11.634521&amp;mra=ls&amp;ie=UTF8&amp;t=m&amp;ll=33.751034,-84.370365&amp;spn=0.034255,0.054932&amp;z=14&amp;output=embed"></iframe>
+              <br>
+              <div class="span5">
+                <address>
+                  <strong class="muted">James Bond</strong><br>
+                  Peachtree St. NE<br>
+                  Atlanat GA-30339<br>
+                  <strong>Office: </strong> 989-999-9999<br>
+                  <strong>Fax: </strong> 999-999-9000<br>
+                  <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top">jame@gmail.com</a><br>
+                  <a href="#">www.ncdd.com</a><br>
+                </address>
+              </div>
+              <div class="span5">
+                <address>
+                  <strong class="muted">James Bond</strong><br>
+                  Peachtree St. NE<br>
+                  Atlanat GA-30339<br>
+                  <strong>Office: </strong> 989-999-9999<br>
+                  <strong>Fax: </strong> 999-999-9000<br>
+                  <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top">jame@gmail.com</a><br>
+                  <a href="#">www.ncdd.com</a><br>
+                </address>
+              </div>
+           </div>
+    <div class="span6">
+      <h3 class="blue1">ABOUT ME</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut ante quam. In nec enim lacus, vel porttitor nibh. Cras iaculis, nunc sed convallis ultrices, erat dolor hendrerit mauris, sed rutrum velit tellus ut metus. Phasellus et tortor mauris, quis consectetur metus. Maecenas quis velit dolor. Integer tincidunt metus sed lorem dictum vel consequat justo suscipit. Nam imperdiet rutrum mollis. Donec vel lacus eu risus luctus 
+        sollicitudin sit amet eu diam. Integer egestas luctus lectus, et pellentesque tellus dignissim vel.
+    </p>
+    <p>
+        Nunc eu lacinia turpis. Nam semper posuere orci. Aliquam erat volutpat. Nunc lectus diam, pulvinar eu egestas scelerisque, laoreet a est. Sed vel hendrerit ante. Praesent vestibulum ornare interdum. Nulla mattis facilisis est vel blandit. Etiam dictum arcu vitae justo cursus ac sagittis leo pulvinar. Etiam quis arcu dolor.
+    </p>
+    </div>
+    </div>
+    <div class="clear"></div>
+                <hr class="hrdotted">
+                <div class="container-fluid">
+          <div class="row-fluid">
+                <div class="span3 borderright">
+                  <h5 class="blue1">Financial:</h5>
+                  <strong>Fees:</strong><br>
+                  Free consultation (60 mins.)
+                  <br>
+                  <strong>Payment:</strong><br>
+                  Cash, Credit Card, Check
+                </div>
+                <div class="span2 borderright">
+                  <h5 class="blue1">Languages:</h5>
+                  <ul>
+                    <li>English</li>
+                    <li>Japanese</li>
+                    <li>Spanish</li>
+                    <li>Hindi</li>
+                   </ul>
+                </div>
+                <div class="span6">
+                  <h4 class="blue1">Practice and Cases:</h4>
+                  <div id="placeholder"  class="demo-placeholder"></div>
+                  <button id="example-4">Label Radius</button>
+                </div>
+                
+                <div class="clear"></div>
+    </div>
+    </div>
+
+    </div>
+    </div>
+    </div>
+    </div>   
+                    <!-- old stuff
                     <? $member = $this->vars['member']; ?>
                     <div class="row-fluid memberProfile">
                         <div class="contactMe pull-right"><a href="mailto:<?=$member['email']?>">CONTACT  ME</a></div>
@@ -150,3 +383,4 @@
                             </div>
                         </div>
                     </div>
+                    old stuff -->
