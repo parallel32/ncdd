9c469af96702ad02fa8cf3b1dbe44bc1e8e4f6e7
Mike Hairetis <parallel32@gmail.com>
2014-02-20 12:20:07 -0500

    updated deploy script to minify js

--- config/deploy.sh
+++ config/deploy.sh
@@ -4,9 +4,9 @@
 #echo "making ssl files......"
 #/home/ec2-user/staging/config/ssl_maker.sh
 
-#echo "minifying......"
+echo "minifying......"
 #/home/ec2-user/staging/config/minify_css.sh
-#/home/ec2-user/staging/config/minify_js.sh
+/home/ec2-user/staging/config/minify_js.sh
 ### temp commented out ###
 
 
@@ -19,9 +19,8 @@ rsync --delete --exclude "logs" --exclude ".gitignore" --exclude ".git" --exclud
 echo "sync web3"
 rsync --delete --exclude "logs" --exclude ".gitignore" --exclude ".git" --exclude "vendor" -rlz /home/ec2-user/staging/ncdd/ ec2-user@web3:/var/www/ncdd
 
-### temp commented out ###
-#echo "discard minified and ssl files for a clean git repo.."
-#git clean -f
-#git checkout src/views/_elements/js/
+echo "discard minified and ssl files for a clean git repo.."
+git clean -f
+git checkout src/views/_elements/admin/js/
+git checkout src/views/_elements/public/js/
 #git checkout src/views/_elements/css/
-### temp commented out ###
--- config/minify_js.sh
+++ config/minify_js.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILES=./src/views/_elements/js/*
+FILES=./src/views/_elements/admin/js/*
 for fullfile in $FILES
 do
   echo "Processing $fullfile file..."
@@ -8,6 +8,19 @@ do
         filename=${filename%.*}
 #echo "filename: $filename extension: $extension"
 #assumes jsmin is in the home directory of ec2-user on admin-master and mike-remote on dev
-~/jsmin/jsmin < $fullfile > "./src/views/_elements/js/$filename.min.php"
-mv "./src/views/_elements/js/$filename.min.php" "./src/views/_elements/js/$filename.php"
+~/jsmin/jsmin < $fullfile > "./src/views/_elements/admin/js/$filename.min.php"
+#mv "./src/views/_elements/admin/js/$filename.min.php" "./src/views/_elements/admin/js/$filename.php"
+done
+
+FILES=./src/views/_elements/public/js/*
+for fullfile in $FILES
+do
+  echo "Processing $fullfile file..."
+        filename=$(basename "$fullfile")
+        extension=${filename##*.}
+        filename=${filename%.*}
+#echo "filename: $filename extension: $extension"
+#assumes jsmin is in the home directory of ec2-user on admin-master and mike-remote on dev
+~/jsmin/jsmin < $fullfile > "./src/views/_elements/public/js/$filename.min.php"
+#mv "./src/views/_elements/public/js/$filename.min.php" "./src/views/_elements/public/js/$filename.php"
 done
