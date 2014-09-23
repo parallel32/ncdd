f7aef8a5b690ea97de4a556c7bbcc1ea208175a4
Mike Hairetis <parallel32@gmail.com>
2014-09-23 09:11:59 -0400

    fixed a bug in the applications results

--- src/controllers/admin/c.application.php
+++ src/controllers/admin/c.application.php
@@ -1150,7 +1150,6 @@ $app->get('/applications/all/{offset}/{limit}', function ($offset, $limit, Reque
 $app->get('/applications/{offset}/{limit}', function ($offset, $limit, Request $request) use ($app) {
 	$application = new Model\Apply($doc=array(), $app);
 	$submitted = $application->fetchByStatus('SUBMITTED',$offset, $limit,$filter=array('type'=>array('$in'=>array('NEW MEMBER APPLICATION','NEW SUSTAINING MEMBER APPLICATION'))));
-	$reference = new \stdClass();
 	for ($i=0; $i < count($submitted); $i++) { 
 		switch ($submitted[$i]['class']) {
 	    	case 'ApplyNewMember':
@@ -1161,7 +1160,7 @@ $app->get('/applications/{offset}/{limit}', function ($offset, $limit, Request $
 	    		break;
 	    	
 	    }
-	    error_log('for variable: reference  ==>'.print_r($reference,true));
+
 	    if(!empty($reference)){
 	    	$submitted[$i]['new_references'] = array('total'=>$reference->getTotalSubmissions(),'max'=>$reference->getMaxSubmissions());
 	    }else{
