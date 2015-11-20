bb70aaa2baca639b0b3d6bbde2bb75c213eac77e
Mike Hairetis <parallel32@gmail.com>
2015-11-20 13:12:27 -0500

    adjusted the csv

--- src/controllers/admin/c.utilities.php
+++ src/controllers/admin/c.utilities.php
@@ -217,7 +217,7 @@ $utilities->get('/memberswithautorenew', function () use ($app) {
         echo implode('|', $value);echo "\r\n";
     }
     echo "<pre>final_arr2:";print_r(count($final_arr2));echo "</pre>";
-    foreach ($final_arr as $value) {
+    foreach ($final_arr2 as $value) {
         echo implode('|', $value);echo "\r\n";
     }
     $final_arr = array_diff_key($final_arr2,$final_arr);
