9ecfce9042acb85210a9bd33ac550551c0b31c0a
Mike Hairetis <parallel32@gmail.com>
2015-12-28 15:43:33 -0500

    disabled the utility for seminar confirmation / welcome email

--- src/controllers/admin/c.utilities.php
+++ src/controllers/admin/c.utilities.php
@@ -19,7 +19,7 @@ $utilities = $app['controllers_factory'];
 
 // put in registration id's and send the confirmation letter to folks who didn't recieve it due to system glitches.
 $utilities->get('/resendseminarconfirmationletter', function () use ($app) {
-    
+    return false;
     $reg_arr[] = '568194f51f1d75597baa2f50';
     // $reg_arr[] = '5669ac51a6ec61ba70f351aa';
     // $reg_arr[] = '5668c017a6ec611874f351aa';
