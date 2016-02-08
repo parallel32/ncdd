e373a569455a066ff73d671ed71722f49cc9be02
Mike Hairetis <parallel32@gmail.com>
2016-02-08 12:11:02 -0500

    upped the batch auto-renew numbers

--- src/controllers/admin/c.application.php
+++ src/controllers/admin/c.application.php
@@ -2840,7 +2840,7 @@ $app->get('/renewalsautocharge', function (Request $request) use ($app) {
 // COMBINE THE ARRAYS since the expYear on the expired cards has been updated 
 		$finalArr = array_merge($valid,$expired);
 
-		for ($x=0; $x < 10; $x++) { 
+		for ($x=0; $x < 25; $x++) { 
 			$value = $finalArr[$x];
 
 			$memberId = $value['record']['_id'];
