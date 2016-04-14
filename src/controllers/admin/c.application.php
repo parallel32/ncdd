a2320015e7816e10671b8353abcc74cd95f88690
Mike Hairetis <parallel32@gmail.com>
2016-04-14 17:31:54 -0400

    disable auto-renew reminder email send controller.

--- src/controllers/admin/c.application.php
+++ src/controllers/admin/c.application.php
@@ -3229,7 +3229,7 @@ echo "<pre>";print_r($value);echo "</pre>";
 
 
 $app->get('/renewals-send-decline-followup-email', function (Request $request) use ($app) {
-//return false;
+return false;
 	$ar = new Model\AutoRenew(array('declined'=>'yes'),$app);
 	$ar_res = $ar->findAllById('declined', $fields=array(), $sort=array(), $slaveOkay=true,$offset=0,$limit=2000);
 	$i=0;
